0206a990a36a22dbd000edc7d5b2b62be2cd5b32
quvvxx <quvvxx2@gmail.com>
2026-07-06 22:26:07 +0900

    faet: 레포지토리 추가

---
+++ src/main/java/com/example/chaeyeon/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.example.chaeyeon.repository;
+
+import com.example.chaeyeon.domain.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
